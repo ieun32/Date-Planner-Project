6a14271935ded46c4fb63d43c373a7d2d47b409a
ieun32 <ueunviss@gmail.com>
2024-05-11 00:39:30 +0900

    ✅ 캘린더 date 변수 의존성 주입

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import "./styles/style.css";
 import * as tags from "./constants/tags";
 import { SCHEDULE_TYPE } from "./constants/schedule";
 
-import DatePlanner from "./utils/datePlanner";
-import HighLighter from "./utils/highlighter";
+// import DatePlanner from "./utils/datePlanner";
+// import HighLighter from "./utils/highlighter";
 import Calender from "./utils/calender";
 class App {
-  constructor({ datePlanner, highLighter, calender }) {
+  constructor() {
+    this.date = new Date();
     this.boyfriend = SCHEDULE_TYPE.dayShift;
     this.girlfriend = SCHEDULE_TYPE.dayShift;
 
-    this.DatePlanner = datePlanner;
-    this.HighLighter = highLighter;
-    this.Calender = calender;
+    // this.DatePlanner = datePlanner;
+    // this.HighLighter = highLighter;
 
     // 셀렉트 이벤트 리스너 등록 (남친, 여친 근무표 업데이트)
     tags.$boyfriendChooser.addEventListener("change", (event) =>
@@ -24,31 +24,34 @@ class App {
     );
 
     // 버튼 이벤트 리스너 등록 (데이트 가능 날짜 계산 후 하이라이팅)
-    tags.$checkDateButton.addEventListener("click", () => {
-      const days = this.DatePlanner.findDay({
-        boyfriend: this.boyfriend,
-        girlfriend: this.girlfriend,
-      });
+    // tags.$checkDateButton.addEventListener("click", () => {
+    //   const days = this.DatePlanner.findDay({
+    //     boyfriend: this.boyfriend,
+    //     girlfriend: this.girlfriend,
+    //   });
 
-      this.HighLighter.initialClassName();
-      this.HighLighter.addClassName(days);
-    });
+    //   this.HighLighter.initialClassName();
+    //   this.HighLighter.addClassName(days);
+    // });
+
+    // 달력 초기화
+    Calender.initial(this.date);
 
     // 달력 이동 버튼 이벤트 리스너 등록
     tags.$prevButton.addEventListener("click", () => {
-      this.Calender.prevMonth();
+      const prevDate = Calender.prevMonth(this.date);
+      this.date = prevDate;
     });
 
     tags.$nextButton.addEventListener("click", () => {
-      this.Calender.nextMonth();
+      const nextDate = Calender.nextMonth(this.date);
+      this.date = nextDate;
     });
 
     tags.$todayButton.addEventListener("click", () => {
-      this.Calender.goToday();
+      Calender.goToday();
+      this.date = new Date();
     });
-
-    // 달력 초기화
-    this.Calender.initial();
   }
 
   /**
@@ -63,12 +66,7 @@ class App {
   }
 }
 
-const datePlanner = new DatePlanner();
-const highLighter = new HighLighter();
-const calender = new Calender();
+// const datePlanner = new DatePlanner();
+// const highLighter = new HighLighter();
 
-new App({
-  datePlanner: datePlanner,
-  highLighter: highLighter,
-  calender: calender,
-});
+new App();
--- src/utils/calender.js
+++ src/utils/calender.js
@@ -2,39 +2,36 @@ import * as tags from "../constants/tags";
 
 // static으로 빼고 싶음 최적화 필요
 export default class Calendar {
-  constructor() {
-    this.date = new Date();
-    this.curDate = new Date();
-  }
+  static initial(date) {
+    const viewYear = date.getFullYear(); // 현재 년도
+    const viewMonth = date.getMonth(); // 현재 월
 
-  initial() {
-    const viewYear = this.date.getFullYear();
-    const viewMonth = this.date.getMonth();
-
-    const prevLast = new Date(viewYear, viewMonth, 0);
-    const thisLast = new Date(viewYear, viewMonth + 1, 0);
+    const prevLast = new Date(viewYear, viewMonth, 0); // 지난 달 마지막 일자 (YYYYMMDD)
+    const thisLast = new Date(viewYear, viewMonth + 1, 0); // 이번 달 마지막 일자 (YYYYMMDD)
 
     const prevDates = [];
-    const thisDates = [...Array(thisLast.getDate()).keys()].map((i) => i + 1);
+    const thisDates = [...Array(thisLast.getDate()).keys()].map((i) => i + 1); // 이번 달 마지막 날짜 개수만큼 일자 배열 만들기
     const nextDates = [];
 
-    const PLDate = prevLast.getDate();
-    const PLDay = prevLast.getDay();
-    const TLDate = thisLast.getDate();
-    const TLDay = thisLast.getDay();
+    const PLDate = prevLast.getDate(); // 지난 달 마지막 날 (DD)
+    const PLDay = prevLast.getDay(); // 지난 달 마지막 날 요일 (0~6)
+    const TLDate = thisLast.getDate(); // 이번 달 마지막 날 (DD)
+    const TLDay = thisLast.getDay(); // 이번 달 마지막 날 요일 (0~6)
     const dates = [];
 
     // 캘린더 헤드에 연도와 월 설정
     tags.$calenderHeadDate.textContent = `${viewYear}년 ${viewMonth + 1}월`;
 
-    // 이전 달의 날짜
+    // 지난 달 마지막 날이 토요일이 아닐 경우
+    // 이번 달 캘린더에 지난 달 일자 삽입 위해 prevDates 배열에 (마지막 날 - 0), (마지막 날 - 1)... 을 unshift
     if (PLDay !== 6) {
       for (let i = 0; i < PLDay + 1; i++) {
         prevDates.unshift(PLDate - i);
       }
     }
 
-    // 다음 달의 날짜
+    // 이번 달 마지막 날이 토요일이 아닐 경우
+    // 이번 달 캘린더에 다음 달 일자 삽입 위해 nextDates 배열에 (7 - 마지막 요일) - 1 개수 만큼 push
     for (let i = 1; i < 7 - TLDay; i++) {
       nextDates.push(i);
     }
@@ -84,18 +81,27 @@ export default class Calendar {
     });
   }
 
-  prevMonth() {
-    this.date.setMonth(this.date.getMonth() - 1);
-    this.initial();
+  // 현재 날짜 정보를 주입 받고 이후 날짜 정보를 초기화 함수에 다시 주입한다.
+  // 초기화 함수는 주입받은 날짜를 기준으로 UI를 변경해준다.
+  static prevMonth(date) {
+    const prevMonth = date.setMonth(date.getMonth() - 1);
+    const prevDate = new Date(prevMonth);
+    Calendar.initial(prevDate);
+    return prevDate;
   }
 
-  nextMonth() {
-    this.date.setMonth(this.date.getMonth() + 1);
-    this.initial();
+  static nextMonth(date) {
+    const nextMonth = date.setMonth(date.getMonth() + 1);
+    const nextDate = new Date(nextMonth);
+    Calendar.initial(nextDate);
+    return nextDate;
   }
 
-  goToday() {
-    this.date = new Date();
-    this.initial();
+  static goToday() {
+    Calendar.initial(new Date());
+  }
+
+  hi(date) {
+    console.log(date);
   }
 }
